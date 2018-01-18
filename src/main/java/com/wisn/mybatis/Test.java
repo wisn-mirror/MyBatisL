7920b50c7ae38f57c1cefcba60300b9535800f5a
Wisn <wuyishun_kmk@outlook.com>
2018-01-18 13:20:23 +0800

    last commit

--- src/main/java/com/wisn/mybatis/Test.java
+++ src/main/java/com/wisn/mybatis/Test.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-//        insertUser();
-        updateUser();
+        insertUser();
+//        updateUser();
 //        selectAllUser();
 //        deleteUser();
 //        selectAllUser();
